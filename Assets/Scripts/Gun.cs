17cd9e8949eb76806d643311c057e1d20f5bafe4
Matt Powell <mpowell171@gmail.com>
2022-12-20 18:21:44 -0600

    Added base functionality for gun script additional work needs to be done, but currently testing if the current method works

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] protected GameObject projectile;
+    [SerializeField] protected Transform gunPoint;
+
     [SerializeField] protected int ammoCount;
     [SerializeField] protected float rateOfFire;
     [SerializeField] protected float reloadSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     protected void ShootGun()
     {
         // Left Click fires weapon
         if (Input.GetMouseButton(0))
         {
-            
+            Instantiate(projectile, gunPoint.position, projectile.transform.rotation);
         }
     }
 }
